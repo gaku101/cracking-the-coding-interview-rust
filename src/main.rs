8234c668a4b8bad3ca5069b0a06ec1584c877b2a
gaku101 <takehiro-r133@hotmail.co.jp>
2025-04-22 16:25:22 +0900

    add 5.4

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,8 @@
+// main.rs
 mod q_5_3;
-
-use q_5_3::{get_next_larger, get_next_smaller};
+use q_5_3::flip_bit_to_win;
 
 fn main() {
-    // サンプル例
-    // 例として n = 13948 を用いる（2進数で表すと 0b11011001111100）
-    let n: u32 = 13948;
-    println!("元の数: {} -> {:b}", n, n);
-
-    match get_next_larger(n) {
-        Some(next_larger) => println!("次に大きい数: {} -> {:b}", next_larger, next_larger),
-        None => println!("次に大きい数は存在しません"),
-    }
-
-    match get_next_smaller(n) {
-        Some(next_smaller) => println!("次に小さい数: {} -> {:b}", next_smaller, next_smaller),
-        None => println!("次に小さい数は存在しません"),
-    }
+    let n = 0b11011101111; // 1775
+    println!("{}", flip_bit_to_win(n)); // 出力: 8
 }
